692970ace8378b3d7d3e868a1f4423da1437543c
roryoreilly <roryoreilly2@gmail.com>
2014-07-26 00:08:39 +0100

    Fade between menu items
    in each item in sushi and salad menu an item is faded out and replaced
    by another faded in item

--- KenKou/MobileSite/main.js
+++ KenKou/MobileSite/main.js
@@ -48,4 +48,31 @@ $(document).ready(function(){
 			closeMenu();
 		}
 	})
+
+	var sushiCount = 0, saladCount =0, amount = 5, delay = 5000, fade = 1000;
+	
+	$(".menu0,.menu1,.menu2").fadeIn(0);
+	$(".menu3,.menu4,.menu5").fadeOut(0);
+	$(".menuItem0,.menuItem1,.menuItem2").fadeIn(0);
+	$(".menuItem3,.menuItem4,.menuItem5").fadeOut(0);
+
+	setTimeout(sushiCycle, delay);
+	function sushiCycle() {
+       $(".menu" + sushiCount%6).fadeOut(fade, function() {
+           $(".menu" +(sushiCount+3)%6).fadeIn(fade, function() {
+            	sushiCount++;
+               	setTimeout(sushiCycle, delay);   
+            });
+        });
+    }
+
+    setTimeout(saladCycle, delay);
+	function saladCycle() {
+       $(".menuItem" + saladCount%6).fadeOut(fade, function() {
+           $(".menuItem" +(saladCount+3)%6).fadeIn(fade, function() {
+            	saladCount++;
+               	setTimeout(saladCycle, delay);   
+            });
+        });
+    }
 })
